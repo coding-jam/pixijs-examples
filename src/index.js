dcc42bc3cb3792a48ca3f6a79c8aca5e8d269bcf
Francesco Strazzullo <francesco.strazzullo86@gmail.com>
2017-02-07 22:31:44 +0100

    added greyscale animation

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
-import { Application, loader, Sprite } from 'pixi.js'
+import { Application, loader, Sprite, filters } from 'pixi.js'
 
 // Create the renderer
 const app = new Application(window.innerWidth, window.innerHeight, {backgroundColor: 0x323B44})
+
+let count = 0
 
 // Fullscreen
 app.renderer.autoResize = true
@@ -17,6 +19,12 @@ loader.add(LOGO_URL).load(() => {
   // Center image
   logo.x = (app.renderer.width - logo.width) / 2
   logo.y = (app.renderer.height - logo.height) / 2
+  logo.filters = [new filters.ColorMatrixFilter()]
+
+  app.ticker.add(() => {
+    count += 0.01
+    logo.filters[0].greyscale(Math.abs(Math.sin(count)), false)
+  })
 
   app.stage.addChild(logo)
 })
