0623014942ab1379b1d0e1dd123f277c5b43e70f
Francesco Strazzullo <francesco.strazzullo86@gmail.com>
2017-02-07 22:39:27 +0100

    added rotation

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { Application, loader, Sprite, filters } from 'pixi.js'
 const app = new Application(window.innerWidth, window.innerHeight, {backgroundColor: 0x323B44})
 
 let count = 0
+let shouldRotate = false
 
 // Fullscreen
 app.renderer.autoResize = true
@@ -19,9 +20,17 @@ loader.add(LOGO_URL).load(() => {
   // Center image
   logo.x = (app.renderer.width - logo.width) / 2
   logo.y = (app.renderer.height - logo.height) / 2
+
+  logo.interactive = true
+  logo.on('click', () => { shouldRotate = !shouldRotate })
+
   logo.filters = [new filters.ColorMatrixFilter()]
 
-  app.ticker.add(() => {
+  app.ticker.add(delta => {
+    if (shouldRotate) {
+      logo.rotation -= 0.01 * delta
+    }
+
     count += 0.01
     logo.filters[0].greyscale(Math.abs(Math.sin(count)), false)
   })
